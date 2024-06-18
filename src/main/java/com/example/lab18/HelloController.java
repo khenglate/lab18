d8dc7f72b85dc57a4b96366d3562705adbcd7f21
Khenglate <solomon2709@gmail.com>
2024-06-18 10:14:06 +0900

    Initial commit

---
+++ src/main/java/com/example/lab18/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.lab18;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+public class HelloController {
+    @FXML
+    private TextArea area;
+    @FXML
+    private TextField text;
+    @FXML
+    public void sendMess(ActionEvent event) {
+        area.appendText("Your massage: " + text.getText() + "\n");
+        text.setText("");
+        text.requestFocus();
+    }
+    public void sendMess_enter(KeyEvent e) {
+        if (e.getCode() == KeyCode.ENTER) {
+        }
+    }
+}
